2c73451aa4a8df580b017210cab39a3b83221615
Licks92 <licks92@gmail.com>
2012-06-23 20:26:26 +0200

    Quatrième partie de la re-rédaction ducode

--- net/minecraft/LoginFormThread.java
+++ net/minecraft/LoginFormThread.java
@@ -6,23 +6,23 @@ import javax.swing.JTextPane;
 
 public class LoginFormThread extends Thread
 {
- private JTextPane editorPane;
- 
- public LoginFormThread(JTextPane editorPane)
- {
-  this.editorPane = editorPane;
- }
+	private JTextPane editorPane;
+
+	public LoginFormThread(JTextPane editorPane)
+	{
+		this.editorPane = editorPane;
+	}
  
     public void run()
     {
         try
         {
-         editorPane.setPage(new URL("https://dl.dropbox.com/u/87115331/LauncherUnicraft/News.html"));
+        	editorPane.setPage(new URL("https://dl.dropbox.com/u/87115331/LauncherUnicraft/News.html"));
         }
         catch (Exception e)
         {
-         e.printStackTrace();
-         editorPane.setText("Impossible de charger les news du serveur..." + e.toString() + "</center></font></body></html>");
+        	e.printStackTrace();
+        	editorPane.setText("Impossible de charger les news du serveur..." + e.toString() + "</center></font></body></html>");
         }
-      }
+    }
 }
